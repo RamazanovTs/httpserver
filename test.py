53f49ca78d32851aeeed349b8a9f5effaafac2af
Hikmet Ramazanov <hikmetramazanov@outlook.com>
2024-10-26 23:17:35 +0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+from server import Server
+
+app=Server()
+app.host="127.0.0.1"
+app.port=8080
+app.template_dir="templates"
+app.template_dic={
+    "index":"index.html",
+    "about":"about.html",
+    "contact":"contact.html",
+}
+app.error_page="404.html"
+
+app.startServer()
